8b133096ad0af870e6593c506dd68fb32f8026c1
jpricardo <joaopds.ricardo@gmail.com>
2025-12-14 18:32:43 -0300

    feat: blur filter

--- cmd/data/filter.go
+++ cmd/data/filter.go
@@ -7,6 +7,7 @@ import (
 type Filter struct {
 	name    string
 	ammount float64
+	radius  int
 }
 
 func Contrast(ammount float64) Filter {
@@ -47,8 +48,7 @@ func applyContrastTo(t *Texture, ammount float64) *Texture {
 				d := 1 - (float64(v) / float64(max))
 				c := d * ammount * (255 - float64(max))
 				nv = v + uint8(c)
-			}
-			if v < mean {
+			} else {
 				d := (float64(v) / float64(min))
 				c := d * ammount * -float64(min)
 				nv = v + uint8(c)
@@ -61,12 +61,56 @@ func applyContrastTo(t *Texture, ammount float64) *Texture {
 	return t
 }
 
+func Blur(ammount float64, radius int) Filter {
+
+	return Filter{name: "blur", ammount: ammount, radius: radius}
+}
+
+func applyBlurTo(t *Texture, ammount float64, radius int) *Texture {
+	size := len(*t)
+
+	for x := range size {
+		for y := range size {
+			total := 0
+			count := 0
+
+			for rx := x - radius; rx < x+radius; rx++ {
+				for ry := y - radius; ry < y+radius; ry++ {
+					if rx < 0 || rx >= size {
+						continue
+					}
+
+					if ry < 0 || ry >= size {
+						continue
+					}
+
+					total += int((*t)[rx][ry])
+					count++
+				}
+			}
+
+			mean := total / count
+			c := (*t)[x][y]
+			d := 1 - float64(c)/float64(mean)
+			vc := d * float64(mean) * ammount
+			nv := uint8(float64(c) + vc)
+
+			(*t)[x][y] = nv
+		}
+	}
+
+	return t
+}
+
 func (f *Filter) ApplyTo(t *Texture) *Texture {
-	fmt.Printf("[FILTER] Applying %f %s filter...\n", f.ammount, f.name)
+	fmt.Printf("[FILTER] Applying filter %+v ...\n", *f)
 
 	switch f.name {
 	case "contrast":
 		applyContrastTo(t, f.ammount)
+
+	case "blur":
+		applyBlurTo(t, f.ammount, f.radius)
 	}
 
 	return t
